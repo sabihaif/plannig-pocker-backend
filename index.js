ba4cb5b432f0a59fc2049c9217458e57a9651ca6
Sabiha Özkan <sabihaiffet@gmail.com>
2022-12-21 16:17:30 +0300

    add socket.io

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 import express  from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
 import baseRouter from "./routes/base.js";
 import roomRouter from "./routes/room.js";
 
 const PORT = process.env.PORT || 5000;
 
 const app = express()
+const server = createServer(app)
+
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+    console.log("A new connection")
+})
 
 app.use('/', baseRouter)
 app.use('/room', roomRouter)
